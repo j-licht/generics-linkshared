40d18ba02b1593f0deafa91d6d2c07e6dd5e2852
Jonas Licht (Riedel) <jonas.licht@riedel.net>
2023-12-19 16:44:53 +0100

    drop dbus

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/j-licht/generics-linkshared
 
 go 1.21.3
-
-require github.com/godbus/dbus/v5 v5.0.4
--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"github.com/j-licht/generics-linkshared/generic"
-	"github.com/godbus/dbus/v5"
 )
 
 func main() {
-	// include something to be able to do linkshared
-	_ = dbus.AuthAnonymous()
-
 	generic.GenericDemo[bool]()
 }
